d1d3eed62de5773dc34da84dbe5b5f62c15e012b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2012-11-15 13:22:49 -0700

    moved stuff around

---
+++ criteria/src/main/java/com/evolutionnext/hibernate/criteria/App.java
@@ -0,0 +1,13 @@
+package com.evolutionnext.hibernate.criteria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
